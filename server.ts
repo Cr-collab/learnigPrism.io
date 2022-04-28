a8bfc66e0962915c53f7236df2912a1f105e0920
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-04-28 18:07:28 -0300

    relizando adição atraves do metodo create na tabela courses

---
+++ server.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function create() {
+  // usabildade para adição de values na nossas tabelas 
+
+  prisma.courses.create({
+    data: {
+      duration: 100,
+      name: "Curso de Pyton",
+      description: "Melhor Curso de Python"
+    }
+  }).then((data) => console.log(data));
+
+
+}
+
+
+create()
